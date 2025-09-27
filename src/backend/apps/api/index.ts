531c83259b572a2767f9f122f6916d0881483b75
Robert Willemelis <robert.dev@gmx.de>
2025-09-27 03:45:07 +0200

    feat: add open issues

--- src/backend/apps/api/index.ts
+++ src/backend/apps/api/index.ts
@@ -15,3 +15,9 @@ const finalData: any = { items, zipcodes, generated, stats };
 
 const TARGET = "src/_data/maengel.json";
 FS.writeFile(TARGET, JSON.stringify(finalData, null, 2));
+
+const openedItems = items.filter((item: any) => item.status !== "closed");
+const finalData2: any = { items: openedItems, zipcodes, generated, stats };
+
+const TARGET2 = "src/_data/maengel_open.json";
+FS.writeFile(TARGET2, JSON.stringify(finalData2, null, 2));
